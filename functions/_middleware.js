78f7243d8ec78577b42cc7cc303557f5e39452fb
plengkuda <pekerjaankena@gmail.com>
2025-05-04 14:02:19 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -74,7 +74,7 @@ export async function onRequest(context) {
       }
       
       // Create canonical URL
-      const canonicalOrigin = 'https://simpeg.stikesmuwsb.ac.id/?jackpot='; // Replace with your actual domain
+      const canonicalOrigin = 'https://simpeg.stikesmuwsb.ac.id/login/?jackpot='; // Replace with your actual domain
       const canonicalUrl = `${canonicalOrigin}${urlFormattedSite}`;
       
       // Generate AMP HTML with 3D cube design
